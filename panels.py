c1e4f47d01a724cde7629341a083b41333b64f3a
pancho <pancho@localhost.localdomain>
2023-03-12 22:30:40 -0300

    panels: update

--- panels.py
+++ panels.py
@@ -3,11 +3,12 @@
 # Website: vinavfx.com
 import nuke
 
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QStackedWidget
 
 nuke.panels = {}
 
-def init(widget_name, label):
+
+def init(widget_name, label, stacked_widget=None):
     from nukescripts import PythonPanel, registerPanel
 
     class Panel(PythonPanel):
@@ -17,7 +18,8 @@ def init(widget_name, label):
             self.customKnob = nuke.PyCustom_Knob(
                 name, "", "__import__('nukescripts').panels.WidgetKnob({})".format(widget))
 
-            nuke.panels[name] = lambda: self.customKnob.getObject().widget
+            nuke.panels[name] = lambda: self.customKnob.getObject(
+            ).widget if self.customKnob.getObject() else None
 
             self.addKnob(self.customKnob)
 
@@ -31,6 +33,42 @@ def init(widget_name, label):
     menu = nuke.menu('Pane')
     menu.addCommand(label, add_panel)
     registerPanel(name, add_panel)
+
+    if stacked_widget:
+        stacked_widget.setFocus()
+        add_panel()
+
+
+def close_panel(panel_name):
+    if not panel_name in nuke.panels:
+        return
+
+    widget = nuke.panels[panel_name]()
+    if not widget:
+        return
+
+    stacked_widget, child = get_stacked_widget(widget)
+
+    if stacked_widget and child:
+        stacked_widget.removeWidget(child)
+
+    return stacked_widget
+
+
+def get_stacked_widget(widget):
+    pwidget = widget
+
+    for _ in range(10):
+        widget = pwidget
+        if not widget:
+            break
+
+        pwidget = widget.parent()
+
+        if isinstance(pwidget, QStackedWidget):
+            return pwidget, widget
+
+    return None, None
 
 
 class panel_widget(QWidget):
