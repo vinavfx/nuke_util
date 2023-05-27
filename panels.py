901c1040bca04e48f0c3e4392665d15d77c42175
pancho <pancho@localhost.localdomain>
2023-05-27 02:08:16 -0400

    panels: remove WindowStaysOnTopHint

--- panels.py
+++ panels.py
@@ -91,7 +91,7 @@ class float_panel_widget(QWidget):
     def __init__(self):
         super(float_panel_widget, self).__init__()
 
-        self.setWindowFlags(Qt.Tool | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.Tool)
 
     def center_window(self):
         centerPoint = QScreen.availableGeometry(
