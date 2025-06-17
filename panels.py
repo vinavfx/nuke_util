a32bc8ef47a2990e61582a8dafaef3499f086995
Francisco <fcocc77@gmail.com>
2025-06-17 14:40:40 -0400

    panels: PySide6 compatible

--- panels.py
+++ panels.py
@@ -6,9 +6,14 @@
 import inspect
 import nuke
 
-from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QWidget, QStackedWidget, QApplication, QDialog
-from PySide2.QtGui import QScreen
+try:
+    from PySide2.QtCore import Qt
+    from PySide2.QtWidgets import QWidget, QStackedWidget, QApplication, QDialog
+    from PySide2.QtGui import QScreen
+except:
+    from PySide6.QtCore import Qt
+    from PySide6.QtWidgets import QWidget, QStackedWidget, QApplication, QDialog
+    from PySide6.QtGui import QScreen
 
 if not hasattr(nuke, 'panels'):
     nuke.panels = {}
