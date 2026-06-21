6d38063a0b7b92d144fd83e69494e4f2926bffba
Francisco Contreras <fcocc77@gmail.com>
2026-06-21 17:12:04 -0400

    pyside: update

--- pyside.py
+++ pyside.py
@@ -2,11 +2,62 @@
 # Qt Compatibility Layer - PySide2/PySide6
 # -----------------------------------------------------------
 
-try:
-    from PySide2.QtCore import Qt
-    from PySide2.QtWidgets import QWidget, QStackedWidget, QApplication, QDialog
-    from PySide2.QtGui import QScreen
-except ImportError:
-    from PySide6.QtCore import Qt
-    from PySide6.QtWidgets import QWidget, QStackedWidget, QApplication, QDialog
-    from PySide6.QtGui import QScreen
+from PySide6.QtCore import (
+    Qt,
+    QEvent,
+    QSize,
+    QTimer,
+    QRect,
+    QRectF,
+    QPoint,
+    QRegularExpression,
+    QSortFilterProxyModel,
+    QTimeLine,
+)
+
+from PySide6.QtWidgets import (
+    QWidget,
+    QStackedWidget,
+    QApplication,
+    QDialog,
+    QHBoxLayout,
+    QVBoxLayout,
+    QComboBox,
+    QLineEdit,
+    QLabel,
+    QPushButton,
+    QListWidget,
+    QMenu,
+    QListWidgetItem,
+    QSplitter,
+    QTextEdit,
+    QPlainTextEdit,
+    QWidgetAction,
+    QSizePolicy,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QListView,
+    QAbstractItemView,
+    QCheckBox,
+    QSpinBox,
+    QSlider,
+)
+
+from PySide6.QtGui import (
+    QAction,
+    QScreen,
+    QIcon,
+    QColor,
+    QTextCursor,
+    QFont,
+    QTextOption,
+    QPainter,
+    QTextFormat,
+    QPalette,
+    QKeySequence,
+    QCursor,
+    QPixmap,
+    QIntValidator,
+    QTransform,
+    QStandardItemModel,
+)
